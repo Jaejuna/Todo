f1bfbe6c31bcb21cb7e79df822f279fd2888e447
Jaejuna <anayana9988@gmail.com>
2021-01-02 15:20:36 +0900

    DashBoard added MainActivity edited

--- Todo/app/src/main/java/com/example/todo/MainActivity.java
+++ Todo/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.todo;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    AnimationUtils animationUtils;
+    Handler handler;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
