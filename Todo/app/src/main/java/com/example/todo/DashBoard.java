82dcf430335d08aa30ce2afb6604323ff0d71cc2
Jaejuna <anayana9988@gmail.com>
2021-01-02 20:06:38 +0900

    DashBoard made

--- Todo/app/src/main/java/com/example/todo/DashBoard.java
+++ Todo/app/src/main/java/com/example/todo/DashBoard.java
@@ -2,13 +2,77 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class DashBoard extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
+
+
+        }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+
+    private void addItem(View view){
+        EditText input = findViewById(R.id.textView);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Please enter text", Toast.LENGTH_LONG).show();
+        }
     }
 }
