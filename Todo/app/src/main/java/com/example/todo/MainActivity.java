89d732b9f2e25abff1c3e4d0fcd71f6ce72d550f
Jaejuna <anayana9988@gmail.com>
2021-01-02 18:33:31 +0900

    Splash screen done

--- Todo/app/src/main/java/com/example/todo/MainActivity.java
+++ Todo/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,21 +1,46 @@
 package com.example.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    AnimationUtils animationUtils;
-    Handler handler;
-    View view;
+    Animation splashIn, splashOut;
+    ImageView image;
+    TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //Animations
+        splashIn = AnimationUtils.loadAnimation(this,R.anim.splash_in);
+        splashOut = AnimationUtils.loadAnimation(this,R.anim.splash_out);
+
+        //hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+
+        image.setAnimation(splashIn);
+        logo.setAnimation(splashIn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,DashBoard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
     }
 }
