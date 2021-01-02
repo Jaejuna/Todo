2c0a8794c03782d7b896ed4007e4847db838a4b0
Jaejuna <anayana9988@gmail.com>
2021-01-02 20:59:03 +0900

    dashBoard done

--- Todo/app/src/main/java/com/example/todo/DashBoard.java
+++ Todo/app/src/main/java/com/example/todo/DashBoard.java
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
